3dc5718e7610cb2acb34909faed2bde028871c67
G. Jay Kerns <gkerns@ysu.edu>
2017-02-09 01:02:42 -0500

    pkg is working

--- pkg/R/ipsur.R
+++ pkg/R/ipsur.R
@@ -1,14 +1,2 @@
 
 read <- function(x) vignette(as.character(substitute(x)))
-
-ipsur <- function (string, site = "ipsur.org") {
-    paste0 <- function(...) paste(..., sep = "")
-    string <- paste0("http://www.google.com/search?as_q=",
-                     gsub(" ", "+", string))
-    mpp <- paste0("as_sitesearch=", site)
-    qstring <- paste(URLencode(string), mpp, sep = "&")
-    browseURL(qstring)
-    cat(gettext("A search query has been submitted to"), "http://www.google.com\n")
-    cat(gettext("The results page should open in your browser shortly\n"))
-    invisible(qstring)
-}
